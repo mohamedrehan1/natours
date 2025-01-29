2f290bcd8112033e6f7cf513cc0b342a6f47843b
Mohamed Rehan <mohamedrehan54321@gmail.com>
2025-01-29 23:23:15 +0200

    add signin page

--- controllers/authController.js
+++ controllers/authController.js
@@ -59,7 +59,7 @@ exports.login = catchAsync(async (req, res, next) => {
   const user = await User.findOne({ email }).select('+password');
 
   if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(new AppError('Incorrect email or password', 401));
+    return next(new AppError('il or password', 401));
   }
 
   // 3) If everything ok, send token to client
